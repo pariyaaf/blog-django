de635d3c3c62416525da57a7e9c0098e8384a36c
pariyaaf <pariyaafsharpour79@gmail.com>
2024-06-20 20:29:32 +0330

    fixed model of posts - images folders

--- pariya_blog/posts/models.py
+++ pariya_blog/posts/models.py
@@ -15,13 +15,13 @@ class PostModel(BaseModel):
     likes_count = models.IntegerField(default=0)
     # post_date = models.DateTimeField(default=datetime.now, blank=True)
     is_published = models.BooleanField(default=True)
-    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/', blank=True)
 
     def __str__(self) -> str:
         return self.title
