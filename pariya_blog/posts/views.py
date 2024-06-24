dab224bc3725b03e0945bc4ebea296b5f7c1f562
pariyaaf <pariyaafsharpour79@gmail.com>
2024-06-24 19:36:05 +0330

    fetch data from database for posts

--- pariya_blog/posts/views.py
+++ pariya_blog/posts/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-
+from .models import PostModel
+from django.core.paginator import Paginator
 def index(request):
-    return render (request, 'posts/posts.html')
+    posts = PostModel.objects.all()
+
+    paginator = Paginator(posts, 3)
+    page = request.GET.get('page')
+    paged_posts_list = paginator.get_page(page)
+
+    context = {
+        'posts':paged_posts_list
+    }
+
+    return render (request, 'posts/posts.html', context)
 
 def post(request, post_id):
     return render(request, 'posts/post.html')
