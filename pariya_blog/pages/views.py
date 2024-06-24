8a23a21c371a91d3a7ded2436d65c2210bc0faaa
pariyaaf <pariyaafsharpour79@gmail.com>
2024-06-24 20:46:55 +0330

    change index method

--- pariya_blog/pages/views.py
+++ pariya_blog/pages/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from posts.models import PostModel
+
+
 
 def index(request):
-    return render (request, 'pages/index.html')
+    posts = PostModel.objects.order_by('-created_at').filter(is_published=True)[:6]
+    return render (request, 'pages/index.html', {'posts' : posts})
+
 
 def about(request):
     return render (request, 'pages/about.html')
