f162b9b4d9d00695d37171fcfd898b43ca38ceda
pariyaaf <pariyaafsharpour79@gmail.com>
2024-06-27 20:59:12 +0330

    modify comments models

--- pariya_blog/comments/models.py
+++ pariya_blog/comments/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from datetime import datetime
-
+from posts.models import PostModel
 class CommentModel(models.Model):
     user_email = models.CharField(max_length=255)
     user_name = models.CharField(max_length=200)
-    commet_date = models.DateTimeField(auto_now_add=True)
-    post_id = models.IntegerField()
+    comment_date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(PostModel, on_delete=models.CASCADE, related_name='comments')
     comment_text = models.TextField()
+    is_published = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.comment_text
