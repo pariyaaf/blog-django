03b3095dcba81435617211005bb353710ad26834
pariyaaf <pariyaafsharpour79@gmail.com>
2024-06-27 20:58:31 +0330

    add comments to post view

--- pariya_blog/posts/views.py
+++ pariya_blog/posts/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from .models import PostModel
 from django.core.paginator import Paginator
 from django.db.models import Q
+from comments.forms import CommentForm
+from comments.models import CommentModel
+
+
 
 def index(request):
     posts = PostModel.objects.order_by('-created_at').filter(is_published=True)
@@ -16,7 +20,15 @@ def index(request):
 
 def post(request, post_id):
     post = get_object_or_404(PostModel.objects.filter(is_published=True), pk=post_id)
-    return render(request, 'posts/post.html',{'post' : post})
+    form = CommentForm()
+    comments = CommentModel.objects.order_by('-comment_date').filter(is_published=True, post_id=post_id)
+    context = {
+        'post':post,
+        'form':form,
+        'comments':comments
+    }
+
+    return render(request, 'posts/post.html', context)
 
 
 def search(request):
