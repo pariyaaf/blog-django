6016a69643ff61e44a8c3b688d2172fc0a92ec1d
pariyaaf <pariyaafsharpour79@gmail.com>
2024-06-28 13:39:51 +0330

    create_comment method

--- pariya_blog/comments/views.py
+++ pariya_blog/comments/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404,redirect
+from posts.models import PostModel
+from django.http import JsonResponse
+from .forms import CommentForm
 
-# Create your views here.
+
+def create_comment(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.save()
+            return JsonResponse({'success': True, 'message': 'کامنت با موفقیت ثبت شد.'})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors})
+    return JsonResponse({'success': False, 'message': 'روش درخواست غیر معتبر است.'})
