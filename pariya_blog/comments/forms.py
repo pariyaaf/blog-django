89267849c429476fc483eab627d0f0d27371b349
pariyaaf <pariyaafsharpour79@gmail.com>
2024-07-03 20:07:16 +0330

    modify form (required fields

--- pariya_blog/comments/forms.py
+++ pariya_blog/comments/forms.py
@@ -4,11 +4,9 @@ from .models import CommentModel
 class CommentForm(forms.ModelForm):
     class Meta:
         model = CommentModel
-        fields = ['user_email', 'comment_text', 'user_name', 'post']  # فیلدهایی که در فرم استفاده می‌شوند باید با مدل مطابقت داشته باشند
+        fields = ['comment_text', 'post']  # فیلدهایی که در فرم استفاده می‌شوند باید با مدل مطابقت داشته باشند
         labels = {
-            'user_email': 'ایمیل',
             'comment_text': 'متن کامنت',
-            'user_name': 'نام کاربری',
         }
         widgets = {
             'post': forms.HiddenInput(),
