a87f02b403aeb79681e655738055f08efb594997
pariyaaf <pariyaafsharpour79@gmail.com>
2024-07-03 20:07:58 +0330

    modify model- add parent

--- pariya_blog/comments/models.py
+++ pariya_blog/comments/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from datetime import datetime
 from posts.models import PostModel
-class CommentModel(models.Model):
+from common.models import BaseModel
+
+
+class CommentModel(BaseModel):
     user_email = models.CharField(max_length=255)
     user_name = models.CharField(max_length=200)
     comment_date = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey(PostModel, on_delete=models.CASCADE, related_name='comments')
     comment_text = models.TextField()
     is_published = models.BooleanField(default=False)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='replies', on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.comment_text
